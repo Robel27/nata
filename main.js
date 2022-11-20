6998e7f181c2cee9820f5c2303b6ecf2da2c8095
Robel27 <99607705+Robel27@users.noreply.github.com>
2022-11-20 15:41:25 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {
+}
+
+function setup() {
+	canvas = createCanvas(640, 480);
+	canvas.position(110, 250);
+	video = createCapture(VIDEO);
+	video.hide();
+}
+
+function take_snapshot(){
+	save('student_name.png');
+}
